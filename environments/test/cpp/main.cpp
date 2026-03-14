36b2f7a0b053354f33e2b9524d4373866d93944a
tour-zotz <>
2026-03-15 01:33:39 +0530

    permission issues, so changed config path

---
+++ environments/test/cpp/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void solution(){
+    int input;
+    cin >> input;
+    cout << input + 1 << endl;
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        solution();
+    }
+}
